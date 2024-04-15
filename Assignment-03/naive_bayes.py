21b6ecf28c241eb908801cee582dc2f92fdd0562
Subham Panda <subhampanda@cpp.com>
2024-04-14 20:47:58 -0700

    Pushed Assignment-03

--- Assignment-03/naive_bayes.py
+++ Assignment-03/naive_bayes.py
@@ -15,33 +15,37 @@ import numpy as np
 #11 classes after discretization
 classes = [i for i in range(-22, 40, 6)]
 
+
 #reading the training data
 #--> add your Python code here
-# reading the training data
-df = pd.read_csv("weather_training.csv", sep=',', header=0)
-X_training = np.array(df.values)[:, 1:-1].astype('f')
+training_data = pd.read_csv('weather_training.csv')
+
 #update the training class values according to the discretization (11 values only)
-#--> add your Python code here
-train = np.array(df.values)[:, -1].astype('f')
-y_training=[]
-for y in train:
-    for cl in classes:
-        if cl - 3 <= y <= cl + 3:
-            y_training.append(cl)
-            break
+def discretizer(data):
+    point_ini = -110
+    for c in classes:
+        if data["Temperature (C)"] > point_ini and data["Temperature (C)"] <= c:
+            data["Temperature (C)"] = c
+        point_ini = c
+    return data
+
+discrete_training = training_data.apply(discretizer, axis = 1)
+y_training = np.array(discrete_training["Temperature (C)"])
+y_training = y_training.astype(dtype='int')
+X_training = np.array(discrete_training.drop(["Temperature (C)","Formatted Date"], axis=1).values)
+
 #reading the test data
 #--> add your Python code here
-df = pd.read_csv("weather_test.csv", sep=',', header=0)
-X_test = np.array(df.values)[:, 1:-1].astype('f')
+test_data = pd.read_csv('weather_test.csv')
+#test_data.dropna(how="all")
+
 #update the test class values according to the discretization (11 values only)
-#--> add your Python code here
-test = np.array(df.values)[:, -1].astype('f')
-y_test = []
-for y in test:
-    for cl in classes:
-        if cl - 3 <= y <= cl + 3:
-            y_test.append(cl)
-            break
+discrete_test = test_data.apply(discretizer, axis = 1)
+y_test = discrete_test["Temperature (C)"]
+y_test = y_test.astype(dtype='int')
+X_test = discrete_test.drop(["Temperature (C)","Formatted Date"], axis=1).values
+
+
 #fitting the naive_bayes to the data
 clf = GaussianNB()
 clf = clf.fit(X_training, y_training)
@@ -49,13 +53,15 @@ clf = clf.fit(X_training, y_training)
 #make the naive_bayes prediction for each test sample and start computing its accuracy
 #the prediction should be considered correct if the output value is [-15%,+15%] distant from the real output values
 #to calculate the % difference between the prediction and the real output values use: 100*(|predicted_value - real_value|)/real_value))
+accuracy = 0
+for (x_testSample, y_testSample) in zip(X_test, y_test):
+    prediction = clf.predict(np.array([x_testSample]))
+    #the prediction should be considered correct if the output value is [-15%,+15%] distant from the real output values.
+    #to calculate the % difference between the prediction and the real output values use: 100*(|predicted_value - real_value|)/real_value))
+    diff = 100*(abs(prediction[0] - y_testSample)/y_testSample)
+    if diff >= -15 and diff <= 15:
+        accuracy+=1
+
 #--> add your Python code here
-accuracy=0
-for (X_testSample, y_testSample) in zip(X_test, y_test):
-    prediction = clf.predict([X_testSample])
-    if (abs(prediction[0]) - abs(y_testSample)) == 0:
-        accuracy += 1
-accuracy=accuracy/len(y_test)
-#print the naive_bayes accuracyy
-#--> add your Python code here
-print("naive_bayes accuracy: " + str(accuracy))
+result = accuracy/len(y_test)
+print(f"naive_bayes accuracy: {result}")
